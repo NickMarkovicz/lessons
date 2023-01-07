4ae8a6c6e13c939ad1aa2d46b4927d97faced4c1
Nick Markovicz <hh.markovich.n@gmail.com>
2023-01-07 15:03:39 +0300

    Added classwork_02.py

---
+++ lesson_06/classwork_02.py
@@ -0,0 +1,40 @@
+# Дан словарь, где в качестве ключей английские слова, а значений - их перевод на русский язык.
+# Написать две функции, одна переводит слово с английского на русский,
+# где слово - это входной параметр, вторая функция - с русского на английский.
+
+# Importing
+import csv
+
+# dictionary = {
+#     "apple": "яблоко",
+#     "car": "машина"
+# }
+
+# Opening dictionary.csv
+with open("dictionary.csv", "r") as doc:
+    reader = csv.reader(doc)
+
+# Defining dictionary
+dictionary = {row[0]: row[1] for row in reader}
+
+
+def translate_to_english(word):  # Translating to English
+    for eng, rus in dictionary.items():
+        if word != rus:
+            continue
+        else:
+            print(f"Слово {rus} на английском: {eng}")
+            return
+    print(f"Слово не найдено")
+
+
+def translate_to_russian(word):  # Translating to Russian
+    if word in dictionary:
+        print(f"Word \"{word}\" in Russian: {dictionary.get(word)}")
+    else:
+        print(f"Word not found")
+
+
+# Calling out functions for print
+translate_to_russian(input(f"Input a word: ").lower())
+translate_to_english(input(f"Введите слово: ").lower())
