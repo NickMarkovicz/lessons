38ce46d259485376bbd1a44fe370824b1c89fda1
Nick Markovicz <hh.markovich.n@gmail.com>
2023-01-11 16:25:50 +0300

    Added commentaries

--- lesson_07/solar_doomsday.py
+++ lesson_07/solar_doomsday.py
@@ -1,19 +1,22 @@
+# Importing
 from random import randint
 from math import floor
 
 
+# Defining function
 def solution(area):
-    panels = [int(floor(area ** 0.5) ** 2)]
-    area -= panels[0]
+    panels = [int(floor(area ** 0.5) ** 2)]  # Making a list with one element inside: the largest square possible
+    area -= panels[0]  # Subtracting the largest square out of total area
     if area == 0:
-        return panels
+        return panels  # Returning list as is, if area is a perfect square
     else:
-        for i in range(int(area / 1)):
+        for i in range(int(area / 1)):  # Filling up empty space with 1x1 solar panels
             panels.append(1)
     return panels
 
 
 area = randint(1, 1000000)
 print(f"Given area is: {area}")
-print(f"Solar panels:", end=' ')
-print(f"{solution(area).count((solution(area)[0]))}x: {solution(area)[0]}, {solution(area).count((solution(area)[1]))}x: {solution(area)[1]}")
+print(f"Solar panels:")
+print(f"""Square of {solution(area)[0]} tiles: {solution(area).count((solution(area)[0]))}x
+Square of {solution(area)[1]} tiles: {solution(area).count((solution(area)[1]))}x""")  # Largest square + 1x1 squares
